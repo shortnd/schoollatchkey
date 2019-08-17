439264d499c5153cc0667b9eea43d47e9d0352e5
Collin <ococncol@gmail.com>
2019-08-17 14:10:12 -0400

    trying to create relationship with users who has role of parent

--- app/ChildParent.php
+++ app/ChildParent.php
@@ -3,41 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class ChildParent extends Authenticatable
+class ChildParent extends Model
 {
-    use Notifiable;
+    protected $guarded = [];
+    protected $fillable = ['id'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    // public function children()
-    // {
-    //     return $this->belongsToMany(Child::class);
-    // }
+    public function children()
+    {
+        return $this->hasMany(Child::class);
+    }
 }
