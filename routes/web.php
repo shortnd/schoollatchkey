0241f0577514e8e3abd02a6bab241c4db84ac47c
Collin OConnell <ococncol@gmail.com>
2019-08-28 08:55:13 -0400

    added commented out routes that need to be added and also added new controllers for other features

--- routes/web.php
+++ routes/web.php
@@ -64,4 +64,25 @@ Route::group([
         Route::patch('pm-in', 'ChildCheckinController@pmCheckin')->name('pm-in');
         Route::patch('pm-out', 'ChildCheckinController@pmCheckout')->name('pm-out');
     });
+
+    /**
+     *  Route::get('children/weekly-totals', 'ChildController@weekly_totals')->name('weekly_totals');
+    Route::resource('children', 'ChildController');
+    Route::patch('children/{child}/update-contract', 'ChildController@updateContact')->name('update-contact');
+    Route::get('children/{child}/all-checkins', 'ChildController@all_checkins')->name('all_checkins');
+    Route::post('add-day/{child}', 'ChildCheckinController@addNewCheckins')->name('add_child');
+    Route::patch('am-checkin/{child}', 'ChildCheckinController@am_checkin')->name('am_checkin');
+    Route::patch('pm-checkin/{child}', 'ChildCheckinController@pm_checkin')->name('pm_checkin');
+    Route::patch('pm-checkout/{child}', 'ChildCheckinController@pm_checkout')->name('pm_checkout');
+    // Day for child
+    Route::get('children/{child}/{checkin}', 'ChildCheckinController@show')->name('child_checkin');
+    // Search for past checkins
+    Route::get('{child}/search-form', 'ChildSearchController@index')->name('search-form');
+    Route::get('{child}/search-form/results', 'ChildSearchController@show')->name('search-results');
+    // Payments
+    Route::get('{child}/payment', 'ChildPaymentController@showPaymentForm')->name('show-payment-form');
+    Route::patch('{child}/pay-past-due', 'ChildPaymentController@payPastDue')->name('pay-past-due');
+    Route::patch('{child}/pay-current-week', 'ChildPaymentController@payWeekTotal')->name('pay-current-week');
+
+     */
 });
