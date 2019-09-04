fd1af1af886bc1998a516afb819b588339440067
Collin OConnell <ococncol@gmail.com>
2019-09-04 08:29:26 -0400

    reworking footer

--- resources/views/child/index.blade.php
+++ resources/views/child/index.blade.php
@@ -90,6 +90,11 @@
     </div>
 </div>
 <div class="card-footer">
+    <div class="row">
+        <div class="col-md-4 mb-3"></div>
+        <div class="col-md-4 mb-3"></div>
+        <div class="col-md-4 mb-3"></div>
+    </div>
     {{-- <div class="row">
         <div class="col-md-4 mb-3">
             <h5 class="d-block">AM Checkin Time</h5>
